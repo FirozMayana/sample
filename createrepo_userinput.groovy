974017086862f5446fa611c5d5b7dae46fd8dc5b
jfrogmc <jfrogmc@GLCDV372.vlgdc.visteon.com>
2018-10-16 08:02:22 -0500

    adding userinput file

---
+++ createrepo_userinput.groovy
@@ -0,0 +1,12 @@
+whichArtifactory = userInput (
+    type : "ARTIFACTORY",
+    name : "Which Artifactory",
+    description : "Please specify the Artifactory service on which to create the repository"
+  )
+  
+artifactory(whichArtifactory.name){
+   localRepository("docker-local") {
+     packageType "docker"
+     description "My local Docker registry"
+   }
+}
